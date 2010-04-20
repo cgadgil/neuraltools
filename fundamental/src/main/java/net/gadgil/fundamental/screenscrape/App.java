e51297322a7bad50a104fea5193f38083dfd197f
chetangadgil1 <c.a.gadgil@gmail.com>
2010-04-19 22:33:50 -0700

    updated

---
+++ fundamental/src/main/java/net/gadgil/fundamental/screenscrape/App.java
@@ -0,0 +1,13 @@
+package net.gadgil.fundamental.screenscrape;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
