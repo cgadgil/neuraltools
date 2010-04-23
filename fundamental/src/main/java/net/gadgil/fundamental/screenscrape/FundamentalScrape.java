83373340750956f0a25f6f8db40fc5a6f0d214e4
chetangadgil1 <c.a.gadgil@gmail.com>
2010-04-23 00:16:27 -0700

    updated with jexcel api

--- fundamental/src/main/java/net/gadgil/fundamental/screenscrape/FundamentalScrape.java
+++ fundamental/src/main/java/net/gadgil/fundamental/screenscrape/FundamentalScrape.java
@@ -11,8 +11,9 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class FundamentalScrape {
 
-	public static List<List<String>> getFinancialDataFromMoneyCentral(String symbol,
-			String annualOrQuarterly, String typeOfStatement) throws Throwable {
+	public static List<List<String>> getFinancialDataFromMoneyCentral(
+			String symbol, String annualOrQuarterly, String typeOfStatement)
+			throws Throwable {
 		List<List<String>> theData = new ArrayList<List<String>>();
 		WebClient theWebClient = new WebClient(BrowserVersion.FIREFOX_3);
 		HtmlPage thePage = theWebClient
@@ -29,49 +30,87 @@ public class FundamentalScrape {
 		for (HtmlElement theRow : theRows) {
 			List<HtmlElement> theCells = theRow.getElementsByTagName("td");
 			List<String> row = new ArrayList<String>();
-			if(theCells.size() !=6) {
+			if (theCells.size() != 6) {
 				continue;
 			}
 			for (HtmlElement theCell : theCells) {
-				String theText = theCell.getTextContent().replaceAll(",", "").trim();
-				if(theText.isEmpty() || theText.length() == 1) {
-					if(row.size() > 0) {
+				String theText = theCell.getTextContent().replaceAll(",", "")
+						.trim();
+				if (theText.isEmpty() || theText.length() == 1) {
+					if (row.size() > 0) {
 						System.err.println("Skipping data for " + row);
 					}
 					continue;
 				}
-				//System.out.print(theText + "\t");
-				//System.out.println(theText.getBytes());
+				// System.out.print(theText + "\t");
+				// System.out.println(theText.getBytes());
 				row.add(theText);
 			}
-			if(row.size() == 6) {
+			if (row.size() == 6) {
 				theData.add(row);
-				//System.out.println();
+				// System.out.println();
 			}
 		}
-		//System.out.println(theHtmlElement);
+		// System.out.println(theHtmlElement);
 		return theData;
 	}
 
 	public static void printTabSeparated(List<List<String>> tableData) {
-		for(List<String> theRow: tableData) {
-			for(String theCell: theRow) {
+		for (List<String> theRow : tableData) {
+			for (String theCell : theRow) {
 				System.out.print(theCell + "\t");
 			}
 			System.out.println("END-OF-LINE");
 		}
 	}
+
+	public static List<List<String>> transposeListOfLists(List<List<String>> tableData) {
+		List<List<String>> theData = new ArrayList<List<String>>();
+		if(tableData.size() == 0) {
+			return theData;
+		}
+		int newNumberOfColumns = tableData.size();
+		int newNumberOfRows = tableData.get(0).size();
+		for(int i=0; i<newNumberOfRows; i++) {
+			List<String> theRow = new ArrayList<String>();
+			for(int j=0; j<newNumberOfColumns; j++) {
+				theRow.add(tableData.get(j).get(i));
+			}
+			theData.add(theRow);
+		}
+		return theData;
+	}
 	
+	public static void printTabSeparatedTransposed(String symbol,
+			String annualOrQuarterly) throws Throwable {
+		List<List<String>> theBalanceSheetData = getFinancialDataFromMoneyCentral(
+				symbol, annualOrQuarterly, "Balance");
+		List<List<String>> theCashFlowData = getFinancialDataFromMoneyCentral(
+				symbol, annualOrQuarterly, "CashFlow");
+		List<List<String>> theIncomeStatementData = getFinancialDataFromMoneyCentral(
+				symbol, annualOrQuarterly, "Income");
+
+		for (List<String> theRow : theBalanceSheetData) {
+			for (String theCell : theRow) {
+				System.out.print(theCell + "\t");
+			}
+			System.out.println("END-OF-LINE");
+		}
+	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) throws Throwable {
 		// TODO Auto-generated method stub
-		List<List<String>> theBalanceSheetData = getFinancialDataFromMoneyCentral("C", "Ann", "Balance");
-		List<List<String>> theCashFlowData = getFinancialDataFromMoneyCentral("C", "Ann", "CashFlow");
-		List<List<String>> theIncomeStatementData = getFinancialDataFromMoneyCentral("C", "Ann", "Income");
-		//printTabSeparated(theBalanceSheetData);
-		printTabSeparated(theCashFlowData);
-		//printTabSeparated(theIncomeStatementData);
+		List<List<String>> theBalanceSheetData = getFinancialDataFromMoneyCentral(
+				"CVV", "Ann", "Balance");
+		List<List<String>> theCashFlowData = getFinancialDataFromMoneyCentral(
+				"CVV", "Ann", "CashFlow");
+		List<List<String>> theIncomeStatementData = getFinancialDataFromMoneyCentral(
+				"CVV", "Ann", "Income");
+		// printTabSeparated(theBalanceSheetData);
+		printTabSeparated(transposeListOfLists(theCashFlowData));
+		// printTabSeparated(theIncomeStatementData);
 	}
 }
