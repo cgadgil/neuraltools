cf3958521c0687da83f095320f44f307cda49d81
cgadgil <cgadgil@171.69.144.155>
2010-04-25 13:02:47 -0700

    added new

---
+++ FinancialAnalysis/test/unit/net/gadgil/financial/BalanceSheetTests.groovy
@@ -0,0 +1,17 @@
+package net.gadgil.financial
+
+import grails.test.*
+
+class BalanceSheetTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
