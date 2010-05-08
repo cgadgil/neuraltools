3502a9366af769f46359dc72936108e95619eb06
Chetan Gadgil <c.a.gadgil@gmail.com>
2010-05-08 09:19:45 -0700

    Converted to webapp

---
+++ fundamental/src/main/java/net/gadgil/fundamental/servlets/FundamentalDataServlet.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package net.gadgil.fundamental.servlets;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author cgadgil
+ *
+ */
+public class FundamentalDataServlet extends HttpServlet {
+
+}
