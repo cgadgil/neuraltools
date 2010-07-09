03ae994fac7840d89b4590bd9de5bc7309024c27
Chetan Gadgil <c.a.gadgil@gmail.com>
2010-07-08 19:10:15 -0700

    working regression sample code

--- fundamental/src/main/java/net/gadgil/analysis/regression/RegressionUtil.java
+++ fundamental/src/main/java/net/gadgil/analysis/regression/RegressionUtil.java
@@ -3,9 +3,11 @@
  */
 package net.gadgil.analysis.regression;
 
+import flanagan.analysis.Regression;
+
 /**
  * @author cgadgil
- *
+ * 
  */
 public class RegressionUtil {
 
@@ -14,7 +16,17 @@ public class RegressionUtil {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		double[][] xdata = new double[][] { new double[] { 1, 2, 3, 4, 5 },
+				new double[] { 3, 2, 4, 2, 11 }, new double[] { 4, 2, 4, 2, 1 } };
+		double[] ydata = new double[] { 11, 24, 31, 42, 48 };
+		double[] xd1 = new double[] { 1, 2, 3, 4, 5 };
+		double[] yd1 = new double[] { 21, 32, 45, 51, 59 };
+		Regression theRegression = new Regression(xdata, ydata);
+		theRegression.linearGeneral();
+		// theRegression.enterData(xxData, binWidth)
+		// System.out.println(theRegression.getAdjustedR2());
+		System.out.println(theRegression.getAdjustedR2());
+		//theRegression.print("/tmp/reg.txt");
 	}
 
 }
