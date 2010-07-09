2e36c64770063a62635df73986e87c2056f9f3fb
Chetan Gadgil <c.a.gadgil@gmail.com>
2010-07-08 18:19:34 -0700

    added regression lib

---
+++ fundamental/src/main/java/net/gadgil/analysis/regression/RegressionUtil.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package net.gadgil.analysis.regression;
+
+/**
+ * @author cgadgil
+ *
+ */
+public class RegressionUtil {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
