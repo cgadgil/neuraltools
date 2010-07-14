0ce7565b66a76e32468ef46ed2c73dbdaa997228
Chetan Gadgil <c.a.gadgil@gmail.com>
2010-07-14 12:45:54 -0700

    working regression

--- fundamental/src/main/java/net/gadgil/fundamental/servlets/RegressionServlet.java
+++ fundamental/src/main/java/net/gadgil/fundamental/servlets/RegressionServlet.java
@@ -4,7 +4,9 @@
 package net.gadgil.fundamental.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Arrays;
+import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,19 +14,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 
 import org.apache.commons.math.stat.regression.OLSMultipleLinearRegression;
+
+import flanagan.analysis.Regression;
 
 /**
  * @author cgadgil
  * 
  */
 public class RegressionServlet extends HttpServlet {
-	
+
 	private double[][] transpose(double[][] matrix) {
 		double[][] theTranspose = new double[matrix[0].length][matrix.length];
-		for(int i=0; i<matrix.length; i++) {
-			for(int j=0; j<matrix[0].length; j++) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
 				theTranspose[j][i] = matrix[i][j];
 			}
 		}
@@ -75,18 +80,41 @@ public class RegressionServlet extends HttpServlet {
 		System.out.println(Arrays.toString(yDataTable));
 		// System.out.println(xData);
 		// System.out.println(yData);
+
+		/*
 		OLSMultipleLinearRegression theOLSMLR = new OLSMultipleLinearRegression();
 		theOLSMLR.newSampleData(yDataTable, xDataTable);
-		System.out.println(Arrays.toString(theOLSMLR
-				.estimateRegressionParameters()));
-		System.out.println(theOLSMLR.estimateRegressandVariance());
-		System.out.println(Arrays.toString(theOLSMLR
-				.estimateRegressionParametersVariance()[0]));
-		System.out.println(Arrays.toString(theOLSMLR.estimateResiduals()));
-		theOLSMLR.estimateResiduals();
-		resp.getOutputStream().write("OK".getBytes());
-		resp.getOutputStream().close();
+		System.out.println("estimateRegressionParameters "
+				+ Arrays.toString(theOLSMLR.estimateRegressionParameters()));
+		System.out.println("estimateRegressandVariance "
+				+ theOLSMLR.estimateRegressandVariance());
+		System.out.println("estimateRegressionParametersVariance "
+				+ Arrays.toString(theOLSMLR
+						.estimateRegressionParametersVariance()[0]));
+		System.out.println("estimateResiduals "
+				+ Arrays.toString(theOLSMLR.estimateResiduals()));
+		System.out.println("estimateRegressionParametersStandardErrors "
+				+ Arrays.toString(theOLSMLR
+						.estimateRegressionParametersStandardErrors()));
+						*/
+		Regression theR = new Regression(this.transpose(xDataTable), yDataTable);
+		theR.linearGeneral();
+		// theR.getAdjustedR2();
+		theR.print();
+		JSONObject theJSONObject = new JSONObject();
+		theJSONObject.put("result", this.getResultData(theR));
+		PrintWriter thePrintWriter = resp.getWriter();
+		thePrintWriter.print(theJSONObject.toString());
+		thePrintWriter.close();
 		// super.doGet(req, resp);
+	}
+
+	private HashMap<String, Object> getResultData(Regression regr) {
+		HashMap<String, Object> theResultData = new HashMap<String, Object>();
+		theResultData.put("coefficients", regr.getCoeff());
+		theResultData.put("sample-r-square", regr.getSampleR2());
+		theResultData.put("residuals", regr.getResiduals());
+		return theResultData;
 	}
 
 	@Override
