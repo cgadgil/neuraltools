6d2c38686e78a03f0532d5e5340bdb9041e4589a
Chetan Gadgil <c.a.gadgil@gmail.com>
2010-08-24 19:58:55 -0700

    updated with ability to process multiple regression from stdin

--- scripts/regression.py
+++ scripts/regression.py
@@ -5,8 +5,8 @@ def getRegressionResults(xData, yData):
     #yd = json.dumps([11.2, 24.1, 29.2, 40.8])
     xd = json.dumps(xData)
     yd = json.dumps(yData)
-    print xData
-    print yData
+    #print xData
+    #print yData
     dd = urllib.urlencode({'xData': xd, 'yData': yd})
     params = dd
     headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
@@ -26,7 +26,7 @@ def getRegressionResultsWithIntercept(xData, yData):
 
 #getRegressionResults([[1.0, 1.0, 2.0, 3.0], [1.0, 2.1, 4.4, 5.9], [1.0, 3.6, 5.9, 8.2], [1.0, 5.1, 9.3, 14.6], [1.0, 7.0, 11.0, 17.0]], [11.2, 24.1, 29.2, 40.8, 54.0])
 
-def processCSVInput():
+def printCSVInputSummary():
     csvFile = csv.reader(sys.stdin, delimiter='\t')
     headerRow = csvFile.next()
     numColumns = len(headerRow)
@@ -35,12 +35,22 @@ def processCSVInput():
     for row in csvFile:
         xData.append([float(i) for i in row[:numColumns-1]])
         yData.append(float(row[numColumns-1]))
-    print xData
-    print yData
-    print getRegressionResultsWithIntercept(xData, yData)
+    #print xData
+    #print yData
+    result = getRegressionResultsWithIntercept(xData, yData)['result']
+    regressionParameters = result['regression-parameters']
+    intercept = regressionParameters[0]
+    coeffecients = regressionParameters[1:]
+    print "[%s] = %f" % (headerRow[numColumns - 1], intercept,),
+    for i in range(0, numColumns - 1):
+        print " + ([%s] * %f)" % (headerRow[i], coeffecients[i]),
+    print ""
+    print "R Squared: %f" % (result['r-squared'])
+    print "R Squared (Adj): %f" % (result['adjusted-r-squared'])
+    print "Residuals: %s" % (str(result['residuals']))
 
 if __name__ == '__main__':
-    processCSVInput()
+    printCSVInputSummary()
 
     
     
