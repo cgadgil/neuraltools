38bcf6fe20d1d048b7b2c38e6e5d9722befe6616
Chetan Gadgil <c.a.gadgil@gmail.com>
2010-10-27 23:32:24 -0700

    updated with more fields

--- scripts/zcalc.py
+++ scripts/zcalc.py
@@ -113,7 +113,7 @@ def getCommonDataFields(dataSet):
     # Income Statement
     #
     # 'Operating Income', 'Total Revenue', 'Gross Profit', 'Operating Income', 'Income Before Tax', 'Income After Tax', 'Net Income'
-    incomeStatementFieldNames = ('Operating Income', 'Total Revenue', 'Gross Profit', 'Operating Income', 'Income Before Tax', 'Income After Tax', 'Net Income', 'Depreciation/Amortization', 'Interest Expense (Income) Net Operating', 'Selling/General/Administrative Expenses Total', 'Cost of Revenue Total', 'Research & Development')
+    incomeStatementFieldNames = ('Operating Income', 'Total Revenue', 'Gross Profit', 'Operating Income', 'Income Before Tax', 'Income After Tax', 'Net Income', 'Depreciation/Amortization', 'Interest Expense (Income) Net Operating', 'Selling/General/Administrative Expenses Total', 'Cost of Revenue Total', 'Research & Development', 'Interest Expense (Income) Net Operating', 'Interest Expense Supplemental', 'Interest Income (Expense) Net Non-Operating')
     #
     # Cash Flow Statement
     # 
